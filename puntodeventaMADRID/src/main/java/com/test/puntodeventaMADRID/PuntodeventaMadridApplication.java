ce350c5355ee7b946d3accefc71b177f7e142ed5
Ignacio Jimenez <ignacio.jimenez@solera.com>
2022-12-01 18:08:35 -0600

    Added initial project for Punto de Venta MADRID

---
+++ puntodeventaMADRID/src/main/java/com/test/puntodeventaMADRID/PuntodeventaMadridApplication.java
@@ -0,0 +1,13 @@
+package com.test.puntodeventaMADRID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntodeventaMadridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntodeventaMadridApplication.class, args);
+	}
+
+}
---
+++ puntodeventaMADRID/src/test/java/com/test/puntodeventaMADRID/PuntodeventaMadridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.puntodeventaMADRID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntodeventaMadridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
